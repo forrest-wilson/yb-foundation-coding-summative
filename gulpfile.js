14c2388dbd462758a0b7a4b4b4e19dc6c53c5752
Forrest Wilson <forrest.wilson@me.com>
2017-11-22 12:16:23 +1300

    Comments

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ const gulp = require("gulp"),
     browserSync = require("browser-sync").create(),
     runSequence = require("run-sequence");
 
+// Cleans the dist directory
 gulp.task("clean", () => {
     return gulp.src("./dist/", { read: false })
         .pipe(clean());
 });
 
+// Compiles sass to css
 gulp.task("sass", () => {
     return gulp.src("./src/sass/*.scss")
         .pipe(sourcemaps.init())
@@ -19,7 +21,8 @@ gulp.task("sass", () => {
         .pipe(gulp.dest("./dist/css/"));
 });
 
-gulp.task("file-copy", () => {
+// Copies the HTML files
+gulp.task("html-copy", () => {
     gulp.src("./src/*.html")
         .pipe(gulp.dest("./dist/"));
 });
@@ -31,9 +34,9 @@ gulp.task("browser-sync", () => {
             baseDir: "dist", // Where the server will start from
             index: "index.html" // Default file to load
         },
-        port: 3000,
+        port: 3000, // Default localhost port
         ui: {
-            port: 3001
+            port: 3001 // Default UI localhost port
         },
         notify: false, // Turns off the notification that the browser has connected to BrowserSync server
         browser: [] // Enter a string or an array of strings to start specific browsers i.e. "google chrome", "safari" or "firefox". Keeping it empty will stop any browsers from opening
@@ -48,10 +51,13 @@ gulp.task("reload-browser", () => {
     }));
 });
 
+// Task that builds the dist folder
 gulp.task("build", (cb) => {
-    runSequence("clean", "sass", "file-copy", "reload-browser", cb);
+    runSequence("clean", "sass", "html-copy", "reload-browser", cb);
 });
 
+// Task that watches all files for changes and runs
+// the build task when a file is changed
 gulp.task("start", ["build", "browser-sync"], (cb) => {
     gulp.watch("./src/**/*.*", ["build"], cb);
 });

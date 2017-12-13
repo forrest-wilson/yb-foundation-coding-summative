e46e606960d7d75f77b3d3424f5ee56e4f8f2025
Forrest Wilson <forrest.wilson@me.com>
2017-12-13 14:06:37 +1300

    Animate.css implementation

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,12 @@ $(document).ready(() => {
 
     // Animate.css jquery extension
     $.fn.extend({
-        animateCss: function (animationName, callback) {
-            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            this.addClass('animated ' + animationName).one(animationEnd, function() {
-                $(this).removeClass('animated ' + animationName);
+        animateCss: function(animationName, callback) {
+            var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+            this.addClass("animated " + animationName).one(animationEnd, function() {
+                $(this).removeClass("animated " + animationName);
                 if (callback) {
-                  callback();
+                    callback();
                 }
             });
             return this;
